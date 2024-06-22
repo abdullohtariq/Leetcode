a6b844d5067805c054169578a7c72a6dc56ce721
abdullohtariq <159859272+abdullohtariq@users.noreply.github.com>
2024-06-22 20:16:15 +0500

    Correcting Spelling Mistake

--- 1. Two Sum (Easy)/Two Sum (Brute Force).cpp
+++ 1. Two Sum (Easy)/Two Sum (Brute Force).cpp
@@ -5,7 +5,7 @@ public:
         int size = nums.size();
         for(int i = 0; i < size - 1; i++)
         {
-            for(int j = i+1; j < size ; j++)
+            for(int j = i + 1; j < size ; j++)
             {
                 if(nums[i] + nums[j] == target)
                 {
